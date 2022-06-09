7eea97bb93708eba0ba6fdebc9546dc3c61f3977
Guilherme Nono <guilherme.nono@etec.sp.gov.br>
2022-06-09 20:08:52 -0300

    Ajustes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let actualColor; 
+let actualColor;
 
 function mudar(cor) {
     document.body.style.backgroundColor = cor;
@@ -6,16 +6,38 @@ function mudar(cor) {
     actualColor = cor;
 }
 
+var isRunning = false;
+
 function secret() {
-    document.body.style.animation = "rainbow 10s infinite";
-    var buttons = [];
+    if (!isRunning) {
+        document.body.style.animation = "rainbow 10s infinite";
+        var buttons = [];
 
-    for(let i = 1;i>=5;i++) {
-        buttons.push(document.getElementById(`b${i}`))
+        for (let i = 1; i <= 5; i++) {
+            buttons.push(document.getElementById(`b${i}`))
+        }
+        let count = 0;
+        buttons.forEach((element) => {
+            if (count % 2 == 0) {
+                element.style.animation = 'shake1 2s infinite'
+            } else {
+                element.style.animation = 'shake2 2s infinite'
+            }
+            count++
+        })
+        isRunning = true
+    } else {
+        
+        var buttons = [];
+
+        document.body.style.animation = "none";
+
+        for (let i = 1; i <= 5; i++) {
+            buttons.push(document.getElementById(`b${i}`))
+        }
+        buttons.forEach((element) => {
+            element.style.animation = 'none'
+        })
+        isRunning = false;
     }
-
-    buttons.forEach((element) => {
-        element.style.animation = 'shake 10s infinite'
-    })
-
 }
